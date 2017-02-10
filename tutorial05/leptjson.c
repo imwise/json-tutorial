6f53d1ec6f4c2cb302c22e0caa2e7b8cad3e78db
Tong He (MSR Student-Person Consulting) <v-hetong@microsoft.com>
2017-02-10 17:06:04 +0800

    finish the ex with the help of answers

--- tutorial05/leptjson.c
+++ tutorial05/leptjson.c
@@ -32,7 +32,7 @@ static void* lept_context_push(lept_context* c, size_t size) {
             c->size = LEPT_PARSE_STACK_INIT_SIZE;
         while (c->top + size >= c->size)
             c->size += c->size >> 1;  /* c->size * 1.5 */
-        c->stack = (char*)realloc(c->stack, c->size);
+        c->stack = (char*)realloc(c->stack, c->size); // old pointer will be invalid
     }
     ret = c->stack + c->top;
     c->top += size;
@@ -184,9 +184,10 @@ static int lept_parse_string(lept_context* c, lept_value* v) {
 static int lept_parse_value(lept_context* c, lept_value* v);
 
 static int lept_parse_array(lept_context* c, lept_value* v) {
-    size_t size = 0;
+    size_t size = 0; // mark the number of JSON Elements.
     int ret;
     EXPECT(c, '[');
+	lept_parse_whitespace(c); 
     if (*c->json == ']') {
         c->json++;
         v->type = LEPT_ARRAY;
@@ -195,29 +196,38 @@ static int lept_parse_array(lept_context* c, lept_value* v) {
         return LEPT_PARSE_OK;
     }
     for (;;) {
-        lept_value e;
+	    lept_value e;
         lept_init(&e);
-        if ((ret = lept_parse_value(c, &e)) != LEPT_PARSE_OK)
-            return ret;
+		if ((ret = lept_parse_value(c, &e)) != LEPT_PARSE_OK)
+			break;
+			//return ret;
         memcpy(lept_context_push(c, sizeof(lept_value)), &e, sizeof(lept_value));
         size++;
-        if (*c->json == ',')
-            c->json++;
-        else if (*c->json == ']') {
-            c->json++;
-            v->type = LEPT_ARRAY;
-            v->u.a.size = size;
-            size *= sizeof(lept_value);
-            memcpy(v->u.a.e = (lept_value*)malloc(size), lept_context_pop(c, size), size);
-            return LEPT_PARSE_OK;
-        }
-        else
-            return LEPT_PARSE_MISS_COMMA_OR_SQUARE_BRACKET;
+		lept_parse_whitespace(c);
+		if (*c->json == ','){
+			c->json++;
+			lept_parse_whitespace(c);
+		}
+		else if (*c->json == ']') {
+			c->json++;
+			v->type = LEPT_ARRAY;
+			v->u.a.size = size;
+			size *= sizeof(lept_value);
+			memcpy(v->u.a.e = (lept_value*)malloc(size), lept_context_pop(c, size), size);
+			return LEPT_PARSE_OK;
+		}
+		else {
+			ret = LEPT_PARSE_MISS_COMMA_OR_SQUARE_BRACKET;
+			break;
+		}
     }
+	for (int i = 0; i < size; i++)
+		lept_free((lept_value*)lept_context_pop(c, sizeof(lept_value)));
+	return ret;
 }
 
 static int lept_parse_value(lept_context* c, lept_value* v) {
-    switch (*c->json) {
+	switch (*c->json) {
         case 't':  return lept_parse_literal(c, v, "true", LEPT_TRUE);
         case 'f':  return lept_parse_literal(c, v, "false", LEPT_FALSE);
         case 'n':  return lept_parse_literal(c, v, "null", LEPT_NULL);
@@ -229,6 +239,11 @@ static int lept_parse_value(lept_context* c, lept_value* v) {
 }
 
 int lept_parse(lept_value* v, const char* json) {
+
+#ifdef _WINDOWS
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+
     lept_context c;
     int ret;
     assert(v != NULL);
@@ -236,7 +251,7 @@ int lept_parse(lept_value* v, const char* json) {
     c.stack = NULL;
     c.size = c.top = 0;
     lept_init(v);
-    lept_parse_whitespace(&c);
+    lept_parse_whitespace(&c); 
     if ((ret = lept_parse_value(&c, v)) == LEPT_PARSE_OK) {
         lept_parse_whitespace(&c);
         if (*c.json != '\0') {
@@ -244,7 +259,7 @@ int lept_parse(lept_value* v, const char* json) {
             ret = LEPT_PARSE_ROOT_NOT_SINGULAR;
         }
     }
-    assert(c.top == 0);
+    assert(c.top == 0); // why? it is meaningless, I think
     free(c.stack);
     return ret;
 }
@@ -253,6 +268,11 @@ void lept_free(lept_value* v) {
     assert(v != NULL);
     if (v->type == LEPT_STRING)
         free(v->u.s.s);
+	if (v->type == LEPT_ARRAY) {
+		for (int i = 0; i < v->u.a.size; i++)
+			lept_free(&v->u.a.e[i]);
+		free(v->u.a.e);
+	}
     v->type = LEPT_NULL;
 }
 
